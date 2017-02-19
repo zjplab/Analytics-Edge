b5f2cb1104181734adb6025ce2ef065360d545ab
Abel Zhang <zjplab@gmail.com>
2017-02-19 19:26:03 +0800

    Add files via upload

--- Unit 2 Linear Regression/temp.r
+++ Unit 2 Linear Regression/temp.r
@@ -1,29 +1,30 @@
 setwd("C:/Users/zjpla/OneDrive/Work/R/Unit 2 -Linear Regression")
-Climate=read.csv(("climate_change.csv"))
-str(Climate)
-Climate$Year
-TrainingSet<-subset(Climate,Climate$Year <= 2006)
-View(TrainingSet)
-View(TrainingSet)
-TestSet=subset(Climate,Climate$Year>2006)
-View(Climate)
-View(Climate)
-Model1<-lm(Temp~ MEI + CO2+CH4+N2O + CFC.11 + CFC.12 + TSI + Aerosols , data = Climate)
-summary(Model1)
-Model1<-lm(Temp~ MEI + CO2+CH4+N2O + CFC.11 + CFC.12 + TSI + Aerosols , data = TrainingSet)
-summary(Model1)
-cor(TrainingSet)
-Model2=lm(Temp~ MEI +TSI + Aerosols +N2O ,data=TrainingSet)
-summary(Model2)
-?step
-Modelx<-step(Model2)
-summary(Modelx)
-Predictions<-predict(Modelx,newdata = TestSet)
-SSE=sum( (Predictions-TestSet$Temp)^2 )
-SSR= sum( (mean(TrainingSet$Temp) - TestSet$Temp)^2)
-R2 = 1 - SSE/SST
-SST=SSR
-R2 = 1 - SSE/SST
-r2
-R2
-savehistory("C:/Users/zjpla/OneDrive/Work/R/Unit 2 -Linear Regression/temp.r")
+Training<- read.csv("pisa2009train.csv")
+Test<-read.csv("pisa2009test.csv")
+str(Training)
+tapply(Training$grade,Training$male,mean)
+tapply(Training$readingScore,Training$male,mean)
+pisaTrain=na.omit
+pisaTrain=na.omit(Training)
+pisaTest<-na.omit(Test)
+str(pisaTrain)
+summary(pisaTest)
+str(pisaTest)
+pisaTrain$raceeth = relevel(pisaTrain$raceeth, "White")
+pisaTest$raceeth = relevel(pisaTest$raceeth, "White")
+str(pisaTest)
+ImScore=lm(pisaTrain$readingScore ~.,data =pisaTrain )
+summary(ImScore)
+lmscore<-ImScore
+SSE<- sum( (lmscore$residuals)^2)
+RMSE<- sqrt ( SSE/nrow(pisaTrain))
+RMSE
+predTest<-predict(lmscore,newdata = pisaTest)
+summary(predTest)
+str(predTest)
+SSE=sum( (pisaTest$readingScore- predTest )^2)
+SSR= sqrt(SSE/nrow(pisaTest))
+SSR
+baseline = mean(pisaTrain$readingScore)
+baseline
+savehistory("C:/Users/zjpla/Desktop/temp.r")
